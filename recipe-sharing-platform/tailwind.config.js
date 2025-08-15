b53fbbc251a1adf04a9de00ddc97df95f74ddb3e
7asanmakki <your-email@used-on-github.com>
2025-08-15 06:08:07 +0300

    Fix Tailwind config: add ./public/index.html for ALX check

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,9 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
