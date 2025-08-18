bea599026d326683066f685347f70dbfa67b130d
7asanmakki <your-email@used-on-github.com>
2025-08-18 08:14:02 +0300

    Use useAuth hook in ProtectedRoute for ALX validator

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { Navigate } from 'react-router-dom'
-import { isAuthenticated } from '../auth'
+import useAuth from '../hooks/useAuth'
 
 export default function ProtectedRoute({ children }) {
-  return isAuthenticated() ? children : <Navigate to="/login" />
+  const { isAuthenticated } = useAuth()
+  return isAuthenticated ? children : <Navigate to="/login" />
 }
